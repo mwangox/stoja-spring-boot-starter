de77e252c671d215a51108cac2487d771f8a1193
israelmwangoka <israelmwangoka@outlook.com>
2024-04-14 11:14:35 +0300

    replace spring.factories with imports files

--- src/main/java/io/github/mwangox/StojaAutoConfiguration.java
+++ src/main/java/io/github/mwangox/StojaAutoConfiguration.java
@@ -2,13 +2,13 @@ package io.github.mwangox;
 
 import com.mwangox.stoja.StooClient;
 import com.mwangox.stoja.config.StooConfig;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@AutoConfiguration
 @ConditionalOnClass(StooClient.class)
 @EnableConfigurationProperties(StojaProperties.class)
 public class StojaAutoConfiguration {
